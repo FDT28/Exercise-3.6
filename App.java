2ced8d4a6a110c25a6277135ac489d84ba898c28
Fabio <fabiodtoguchi@gmail.com>
2023-12-05 07:46:44 -0500

    Update

--- App.java
+++ App.java
@@ -2,15 +2,13 @@ import java.util.Scanner;
 
 public class App{
     public static void main(String[]args) {
-        Scanner scannerfeet = new Scanner(System.in);
-        Scanner scannerinches = new Scanner(System.in);
-        Scanner scannerpounds = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your height in feet (ft):");
-        double feet = scannerfeet.nextDouble();
+        double feet = scanner.nextDouble();
         System.out.println("Enter the rest height in inches (in):");
-        double inches = scannerinches.nextDouble() + (feet*12);
+        double inches = scanner.nextDouble() + (feet*12);
         System.out.println("Enter your weight in pounds (lb):");
-        double pounds = scannerpounds.nextDouble();
+        double pounds = scanner.nextDouble();
         double BMI = pounds/Math.pow(inches, 2)*703;
         System.out.println("Your BMI is: " + BMI);
     }
